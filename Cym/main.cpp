cd7c9a37ae443516bd4d15b8fc4d120cc2cf7635
ko-shi <koshi1971@outlook.jp>
2018-03-31 21:33:47 +0900

    ファイルから読み込み
    ファイルからコードを読み込むようにした
    拡張子.cym

--- Cym/main.cpp
+++ Cym/main.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
+#include<fstream>
 #include"Cym.hpp"
 
 int main() {
 	using namespace std::string_literals;
 	using namespace std::string_view_literals;
-	/*
+	
+	std::fstream file("test.cym");
+
 	cym::Cym project;
-	project.addLine(u"#value = 10");
+	std::string str;
+	while (std::getline(file, str)) {
+		project.addLine(cym::to16String(str));
+	}
 	project.compile();
 	project.run();
 	std::cout << cym::toSJisString(project.showMemory());
-	*/
-	const auto a = cym::convertToRPN(u"3 + sum(1,2,3)"sv, cym::Vector<cym::StrView>{u"(", u")", u"+"}, [](const cym::StrView &l, const cym::StrView &r) {
-		static const std::unordered_map<cym::StrView, int> priority = { { u"(",2 },{ u")",2 },{ u"=",16 },{ u"+",6 },{ u"-",6 },{ u"*",5 },{ u"/",5 } };
-		return priority.at(l) > priority.at(r);
-	});
-	std::cout << cym::toSJisString(cym::showRPN<cym::Str>(a));
+	
 }
