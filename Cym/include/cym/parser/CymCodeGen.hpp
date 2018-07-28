b5a42fd016a7c45c38f53f4f5db5bd90edbae646
ko-shi <koshi1971@outlook.jp>
2018-07-29 01:20:16 +0900

    CodeGen ちょっと

--- Cym/include/cym/parser/CymCodeGen.hpp
+++ Cym/include/cym/parser/CymCodeGen.hpp
@@ -5,33 +5,42 @@
 #include<cym/vm/CymOpcode.hpp>
 #include<cym/lang/CymInstancedType.hpp>
 #include<cym/utils/CymForwardIndexMap.hpp>
+#include<cym/utils/CymDoubleKeyMap.hpp>
 
 namespace cym {
 	class CodeGen {
 		ByteCode byte_code_;
 		FuncDef ast_;
+		DoubleKeyMap<Pair<Str,InstancedType>,ByteCodeFunc> defined_operators_;
 		CodeGen(FuncDef &&a) : ast_(std::move(a)) {
 
 		}
-		FuncDef* findFunc(const Vector<Scope> &s,StrView name) {
+		FuncDef* findFunc(const Vector<Scope> &s,const Str &name) {
+			// TODO: オーバーロード対応
 			for (auto i = s.rbegin(); i != s.rend(); i++) {
 				if (i->index() == ScopeKind::FUNC_SCOPE) {
 					const auto &func = std::get<ScopeKind::FUNC_SCOPE>(*i);
-					func->inner_func.
+					if (func->inner_func.count(name)) {
+						return &func->inner_func[name];
+					}
 				}
 			}
 		}
 		void generate() {
 			Vector<Scope> stratum;
 			stratum.emplace_back(&ast_);
+			ByteCodeFunc func_code;
 			for (const auto &i : ast_.order) {
 				switch (i->id()) {
 				case ASTId::CALL_FUNC: {
 					const auto ast = dynamic_cast<ASTCallFunc*>(i.get());
-					ast->name;
+					findFunc(stratum,ast->name);
 				}
 				}
 			}
+		}
+		void generateCallFunc(/* Copy constructing */Vector<Scope> stratum,const ASTCallFunc &func,ByteCodeFunc &caller) {
+			caller.com.push_back
 		}
 	};
 }
